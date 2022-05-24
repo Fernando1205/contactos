dae1c160f7918c0e05c3af81e82edcdd6b8b74fc
Fernando Lopez <fer68769@gmail.com>
2022-05-24 18:36:46 -0500

    Actualizar contacto front

--- public/js/proyectos.js
+++ public/js/proyectos.js
@@ -1,9 +1,30 @@
+const btnSubmit = document.querySelector('#btnSubmit');
+const btnUpdate = document.querySelector('#btnUpdate');
+
+
+// Inputs de formulario
 inpName = document.querySelector('#name');
 inpLastname = document.querySelector('#lastname');
 inpEmail = document.querySelector('#email');
 inpPhone = document.querySelector('#phone');
-modal = $('#createContact');
 
+// Boton modal crear
+btnCreate = document.querySelector('#modalShow');
+
+// Modal
+modal = $('#createContact');
+titleModal = document.querySelector('#createContactLabel');
+
+// Funcion mostrar modal
+btnCreate.addEventListener('click', (e) => {
+    resetModal();
+    modal.modal('show');
+    titleModal.innerHTML = 'Crear nuevo contacto';
+    btnSubmit.style.display = 'block';
+    btnUpdate.style.display = 'none';
+});
+
+// Muestra datatable con datos de api
 var table = $('#custom-table').DataTable({
     ajax: {
         "url": "/list",
@@ -19,7 +40,7 @@ var table = $('#custom-table').DataTable({
             data: "_id",
             render: function(data, type, row, meta) {
                 return type === 'display' ?
-                    `<button class="btn btn-outline-dark btn-sm"><i class="fa-solid fa-pen-to-square"></i></button>
+                    `<button class="btn btn-outline-dark btn-sm editBtn"><i class="fa-solid fa-pen-to-square"></i></button>
                     <button class="btn btn-outline-dark btn-sm"><i class="fa-solid fa-trash"></i></button>` :
                     data;
             }
@@ -47,8 +68,7 @@ var table = $('#custom-table').DataTable({
     },
 });
 
-const btnSubmit = document.querySelector('#btnSubmit');
-
+// Funcion para guardar contacto
 btnSubmit.addEventListener('click', (e) => {
 
     e.preventDefault();
@@ -76,11 +96,74 @@ btnSubmit.addEventListener('click', (e) => {
         .then((data) => {
             successAlert(data.message);
             resetModal();
+            modal.modal('hide');
             table.ajax.reload(null, false);
         })
         .catch((e) => console.log(e))
 })
 
+function resetModal() {
+    document.querySelector('#name').value = "";
+    document.querySelector('#lastname').value = "";
+    document.querySelector('#email').value = "";
+    document.querySelector('#phone').value = "";
+}
+
+// Muestra los datos a editar en el modal
+$('#custom-table tbody').on('click', '.editBtn', function() {
+    // Obtiene los datos de la fila seleccionada
+    var data = table.row($(this).parents('tr')).data();
+
+    // Muestra modal y cambia titulo
+    modal.modal('show');
+    titleModal.innerHTML = 'Editar contacto';
+
+    const { _id, name, lastname, email, phone } = data;
+    inpName.value = name;
+    inpLastname.value = lastname;
+    inpEmail.value = email;
+    inpPhone.value = phone
+    btnSubmit.style.display = 'none';
+    btnUpdate.style.display = 'block';
+    btnUpdate.dataset._id = _id;
+});
+
+// Funcion para guardar contacto
+$('body').on('click', '#btnUpdate', (e) => {
+
+    e.preventDefault();
+    let _id = e.target.dataset._id;
+
+    fetch(`http://127.0.0.1:3001/${_id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: new URLSearchParams({
+                name: inpName.value,
+                lastname: inpLastname.value,
+                email: inpEmail.value,
+                phone: inpPhone.value
+            })
+        })
+        .then((response) => {
+            if (response.status === 200) {
+                return response.json();
+            } else {
+                warningAlert();
+                throw new Error('Ocurrio un error');
+            }
+        })
+        .then((data) => {
+            successAlert(data.message);
+            resetModal();
+            modal.modal('hide');
+            table.ajax.reload(null, false);
+        })
+        .catch((e) => console.log(e))
+})
+
+// SWEETALERTS
 function successAlert(msg) {
     Swal.fire('¡Exito!', msg, 'success')
 }
@@ -88,17 +171,4 @@ function successAlert(msg) {
 function warningAlert() {
     Swal.fire('¡Error!', 'Ha ocurrido un error', 'warning')
 }
-
-function resetModal() {
-    document.querySelector('#name').value = "";
-    document.querySelector('#lastname').value = "";
-    document.querySelector('#email').value = "";
-    document.querySelector('#phone').value = "";
-    modal.modal('hide');
-}
-
-$('#custom-table tbody').on('click', 'button', function() {
-    var data = table.row($(this).parents('tr')).data();
-    console.log(data);
-    alert(data._id + "' Nombre is: " + data.name);
-});
+// SWEETALERTS
