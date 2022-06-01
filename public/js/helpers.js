6c31dcf2f0fd118242935bcd8b9dc6379f8acaea
Fernando Lopez <fer68769@gmail.com>
2022-06-01 13:23:33 -0500

    Se agrego helpers js

---
+++ public/js/helpers.js
@@ -0,0 +1,9 @@
+// SWEETALERTS
+function successAlert(msg) {
+    Swal.fire('¡Exito!', msg, 'success')
+}
+
+function warningAlert() {
+    Swal.fire('¡Error!', 'Ha ocurrido un error', 'warning')
+}
+// SWEETALERTS
