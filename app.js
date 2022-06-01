b5adfa0141b9690bb6fb54c237c5452e9012a32a
Fernando Lopez <fer68769@gmail.com>
2022-06-01 10:00:35 -0500

    Se agrego ruta y vista de registro y se configuro hbs para agregar scripts a plantillas

--- app.js
+++ app.js
@@ -3,12 +3,18 @@ require('./config/db');
 const express = require('express');
 const { create } = require('express-handlebars');
 const { port } = require('./config/config');
-const router = require('./routes/contact');
 
 const app = express();
 const hbs = create({
     extname: ".hbs",
-    partialsDir: __dirname + '/views/partials/'
+    partialsDir: __dirname + '/views/partials/',
+    helpers: {
+        section: function(name, options) {
+            if (!this._sections) this._sections = {};
+            this._sections[name] = options.fn(this);
+            return null;
+        }
+    }
 });
 
 // Handlebars engine
@@ -21,6 +27,7 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/', require('./routes/contact'));
+app.use('/auth', require('./routes/auth'));
 
 app.listen(port, () => {
     console.log(`Servidor corriendo en el puerto ${port} 🚀`);
