9593273ef85c9dac4c2efe31caa38d4d8e79c876
Fernando Lopez <fer68769@gmail.com>
2022-06-01 16:52:38 -0500

    Validación front de registro

--- public/js/register.js
+++ public/js/register.js
@@ -42,10 +42,29 @@ btnRegister.addEventListener('click', function(e) {
         })
 })
 
+// Validación de los inputs del formulario
+
+let inpCampos = ['name', 'lastname', 'email'];
+inpCampos.forEach(inp => {
+    document.querySelector(`#${inp}`).addEventListener('keyup', function() {
+        if (this.value !== '' && this.value.length > 3) {
+            this.classList.remove('is-invalid');
+            document.querySelector(`#${inp}Error`).style.display = 'none';
+        } else {
+            this.classList.add('is-invalid');
+            document.querySelector(`#${inp}Error`).style.display = 'block';
+        }
+    });
+});
+
+// Validación de los inputs del formulario
+
+
 // Muestra errores en los inputs
 function showErrors(errors) {
     errors.forEach(error => {
         document.querySelector(`#${error.param}Error`).style.display = 'block';
+        document.querySelector(`#${error.param}`).classList.add('is-invalid');
         document.querySelector(`#${error.param}Error`).innerHTML = error.msg;
     });
 }
@@ -62,8 +81,12 @@ function resetForm() {
 // Checa si son iguales las contraseñas
 txtPassConfirm.addEventListener('keyup', function() {
     if (this.value == txtPass.value && this.value.length > 5) {
+        inpPass.classList.add('is-valid');
+        inpPassConf.classList.add('is-valid');
         btnRegister.removeAttribute('disabled');
     } else {
+        inpPass.classList.remove('is-valid');
+        inpPassConf.classList.remove('is-valid');
         btnRegister.setAttribute('disabled', true);
     }
 });
@@ -71,8 +94,12 @@ txtPassConfirm.addEventListener('keyup', function() {
 // Checa si son iguales las contraseñas
 txtPass.addEventListener('keyup', function() {
     if (this.value == txtPassConfirm.value && this.value.length > 5) {
+        inpPass.classList.add('is-valid');
+        inpPassConf.classList.add('is-valid');
         btnRegister.removeAttribute('disabled');
     } else {
+        inpPass.classList.remove('is-valid');
+        inpPassConf.classList.remove('is-valid');
         btnRegister.setAttribute('disabled', true);
     }
 });
