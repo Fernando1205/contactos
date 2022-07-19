83c1fcdae785b123015374ffc3bd6fb00395d417
Fernando Lopez <fer68769@gmail.com>
2022-07-19 14:11:58 -0500

    Se agrego middleware a todas las rutas de contacto

--- routes/contact.js
+++ routes/contact.js
@@ -8,20 +8,20 @@ const verificarUser = require('../middleware/verificarUser');
 router.get('/', verificarUser, home);
 router.get('/list', list);
 
-router.post('/', [
+router.post('/', verificarUser, [
     body('name', 'El nombre es requerido').trim().notEmpty().escape(),
     body('lastname', 'El apellido es requerido').trim().notEmpty().escape(),
     body('email', 'El apellido es requerido').trim().isEmail().normalizeEmail(),
     body('phone', 'El apellido es requerido').trim().isNumeric().escape()
 ], store);
 
-router.put('/:id', [
+router.put('/:id', verificarUser, [
     body('name', 'El nombre es requerido').trim().notEmpty().escape(),
     body('lastname', 'El apellido es requerido').trim().notEmpty().escape(),
     body('email', 'El apellido es requerido').trim().isEmail().normalizeEmail(),
     body('phone', 'El apellido es requerido').trim().isNumeric().escape()
 ], update);
 
-router.delete('/:id', destroy);
+router.delete('/:id', verificarUser, destroy);
 
 module.exports = router;
